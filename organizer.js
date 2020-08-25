112be84bdfe7f8d723aeec4174eff165fa7b2428
estevao-bchaves <estevao.bchaves@protonmail.com>
2020-08-25 20:43:59 -0300

    organizer organizado

--- organizer.js
+++ organizer.js
@@ -2,23 +2,23 @@ function funcao() {
 
     let varValue = document.getElementById('varValue').value;
     let varType = document.querySelector('input[name="varType"]:checked').value;
-
-    let dataList = [1,10,5,20,30,5,8,7,6]
-    //varValue.split(/\s*;\s*/); //isolates each value
+    let dataList = varValue.split(/\s*;\s*/); //isolates each value
 
     console.log(dataList);
-
-    dataList.sort((a, b) => a < b ? -1 : a > b ? 1 : 0) //organizes by crescent order
+    if (varType == "Quantitativa Discreta" || "Quantitativa Contínua") {
+        for (i = 0; i < dataList.length; i++) {
+            dataList[i] = Number(dataList[i]);
+        }
+        dataList.sort((a,b)=> a - b);
+    }  else{
+        dataList.sort() //organizes by crescent order
+    }
 
     console.log(dataList);
-
-    dataList.sort((a,b) => b - a);
-
-    console.log(dataList);
-
     var testObject = {}
     dataList.forEach(i => {
         !(i in testObject) ? testObject[i] = 1 :  testObject[i] += 1 //counter
+        //testObject[i] = !(testObject[i]) ? testObject[i] = 1 : testObject[i] +=1
     });
-    //console.log(testObject);
+    console.log(testObject);
 }
