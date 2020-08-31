1df1f08c71f68f2bc9ad33c7731f1caf76a868be
RafaelLaporte <rafanex@gmail.com>
2020-08-31 19:03:49 -0300

    Opção 'Quantitativa Contínua' ajudastada

--- organizer.js
+++ organizer.js
@@ -1,71 +1,58 @@
-function start() {
-    document.getElementById('varName').addEventListener("input", () => {
-        document.querySelectorAll('input[name="varType"]').forEach(radio => {
-            radio.disabled = false;
-        });
-    });
-
-    document.querySelectorAll('input[name="varType"]').forEach(radio => {
-        radio.addEventListener("change", () =>{
-            document.getElementById("varValue").disabled = false
-        });
-    });
-
-    document.getElementById('varValue').addEventListener("input", () => {
-        document.querySelectorAll('input[name="varScope"]').forEach(radio => {
-            radio.disabled = false
-        });
-    });
-}
-
 function calculate() {   
     let varName = document.getElementById('varName').value;
 
-    if (varName.length == 0) {
-        alert("Por favor, preencha todos os campos corretamente");
-    } else {
+    //Reading Variables
+    let varValue = document.getElementById('varValue').value;
+    let varType = document.querySelector('input[name="varType"]:checked').value;
+    //let varScope = document.querySelector('input[name="varScope"]:checked').value;
+    let dataList = varValue.split(/\s*;\s*/); 
+    let valuesQuantity = {};
 
-        //Reading Variables
-        let varValue = document.getElementById('varValue').value;
-        let varType = document.querySelector('input[name="varType"]:checked').value;
-        //let varScope = document.querySelector('input[name="varScope"]:checked').value;
-        let dataList = varValue.split(/\s*;\s*/); 
-        let valuesQuantity = {};
-
-        if (["Quantitativa Discreta", "Quantitativa Contínua"].includes(varType)) {
-            for (i = 0; i < dataList.length; i++) {
-                dataList[i] = Number(dataList[i]);
-            }
-            dataList.sort((a,b) => a - b);
-        } else {
-            dataList.sort();
+    if (["quantitativaDiscreta", "quantitativaContinua"].includes(varType)) {
+        for (i = 0; i < dataList.length; i++) {
+            dataList[i] = Number(dataList[i]);
         }
+        dataList.sort((a,b) => a - b);
+    } else {
+        dataList.sort();
+    }
 
 
-        //Counting
-        dataList.forEach(key => {
-            !(key in valuesQuantity) ? valuesQuantity[key] = 1 :  valuesQuantity[key] += 1
-        });
+    //Counting
+    dataList.forEach(key => {
+        !(key in valuesQuantity) ? valuesQuantity[key] = 1 :  valuesQuantity[key] += 1
+    });
 
 
-        //Generating Tables
-        document.getElementById("tabela").innerHTML += `<tr>
-            <th>Nome da Variável</th>
+    //Generating Tables
+    document.getElementById("thead").innerHTML = `<br>
+            <tr>
+            <th>${varName}</th>
             <th>Frequência</th>
             </tr>`
 
+    if (varType == "qualitativaOrdinal")  {
+
+        document.getElementById("explanation").innerHTML = 
+        "<b style='font-size: 20px;'>Clique e arraste para trocar a posição das linhas.</b>"
+
         for (key in valuesQuantity) {
-            document.getElementById("tabela").innerHTML += `<tr>
-            <td>${key}</td>
-            <td>${valuesQuantity[key]}</td>
-            </tr>`
+            document.getElementById("tbody").innerHTML += 
+                `<tr>
+                <td>${key}</td>
+                <td>${valuesQuantity[key]}</td>
+                </tr>`
         }
 
-        if (varType == "Quantitativa Ordinal") {
-            document.getElementById("moveRows").innerHTML += `<p>Selecione a linha e use os botões a seguir para 
-                                                                move-las.</p>`
-            document.getElementById("moveRows").innerHTML += `<button onlick='moveRows()'>&#x21E7</button>`
-            document.getElementById("moveRows").innerHTML += `<button onlick='moveRows()'>&#x21E8</button>`
-        } 
-    }   
+        document.getElementById("tableScript").innerHTML = "$('tbody').sortable();"
+        
+    } else {
+        for (key in valuesQuantity) {
+            document.getElementById("tbody").innerHTML += 
+                `<tr>
+                <td>${key}</td>
+                <td>${valuesQuantity[key]}</td>
+                </tr>`
+        }
+    }
 }
