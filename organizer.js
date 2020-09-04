4f0736e662d797e570bc765375c2b94785c105bc
RafaelLaporte <rafanex@gmail.com>
2020-09-04 10:38:51 -0300

    Cálculos de frequências adicionados na tabela

--- organizer.js
+++ organizer.js
@@ -3,20 +3,24 @@ function calculate() {
     document.getElementById("thead").innerHTML = "";
     document.getElementById("tbody").innerHTML = "";
 
-    let varName = document.getElementById('varName').value;
+    let valuesQuantity = {};
 
     //Reading Variables
+    let varName = document.getElementById('varName').value;
     let varValue = document.getElementById('varValue').value;
     let varType = document.querySelector('input[name="varType"]:checked').value;
-    let dataList
-    //let varScope = document.querySelector('input[name="varScope"]:checked').value;
-    varValue.split(/\s*;\s*/) == '' || undefined || null ? alert('Erro de digitação das variáveis') : dataList =  varValue.split(/\s*;\s*/); 
-    let valuesQuantity = {};
-    
+    let dataList = varValue.split(/\s*;\s*/);
 
+    for (i = 0; i < dataList.length; i++) {
+        if (dataList[i] === "") {
+            dataList.splice(i,1);
+            i--
+        }
+    }
+    //let varScope = document.querySelector('input[name="varScope"]:checked').value;
+    
     if (["quantitativaDiscreta", "quantitativaContinua"].includes(varType)) {
         for (i = 0; i < dataList.length; i++) {
-            console.log(typeof dataList[i])
             dataList[i] = Number(dataList[i]);
         }
         dataList.sort((a,b) => a - b);
@@ -24,19 +28,24 @@ function calculate() {
         dataList.sort();
     }
 
-
     //Counting
     dataList.forEach(key => {
         !(key in valuesQuantity) ? valuesQuantity[key] = 1 :  valuesQuantity[key] += 1
     });
 
+    let percentFr = calcPercentFr(dataList, valuesQuantity);
+    let fac = calcFac(valuesQuantity);
+    let percentFac = calcPercentFac(percentFr);
 
     //Generating Tables
     document.getElementById("thead").innerHTML = 
             `<tr>
             <td>${varName}</td>
-            <td>Frequência</td>
-            </tr>`
+            <td>Frequência Simples (Fi)</td>
+            <td>Frequência Simples % (Fr%)</td>
+            <td>Frequência acumulada (Fac)</td>
+            <td>Frequência acumulada % (Fac%)</td>
+            </tr>`               
 
     if (varType == "qualitativaOrdinal")  {
 
@@ -48,46 +57,98 @@ function calculate() {
                 `<tr>
                 <td>${key}</td>
                 <td>${valuesQuantity[key]}</td>
+                <td>${percentFr[key]}</td>
+                <td>${fac[key]}</td>
+                <td>${percentFac[key]}</td>
                 </tr>`
         }
 
         document.getElementById("tableScript").innerHTML = "$('tbody').sortable();"
+
+        $('#results tr').each(() => {
+            let teste = $(this).find("td:first").html();  
+            console.log(teste)
+        });
         
     } else if(varType == "quantitativaContinua"){
 
-        let xMin = Math.min(...Object.keys(valuesQuantity))  
-        let xMax = Math.max(...Object.keys(valuesQuantity))
+        let xMin = Math.min(...Object.keys(valuesQuantity));  
+        let xMax = Math.max(...Object.keys(valuesQuantity));
         let amplitude = xMax - xMin 
         let k = 0
-        for (key in dataList) {
-        k += Number(dataList[key])
-    }
-     k = Math.round(Math.sqrt(k)) 
-     let intervalo = Math.ceil(amplitude/k)
-       console.log(intervalo)
-       let xMaxParcial = xMin
-       while (xMaxParcial <= xMax) { 
-           let frequency = 0
+
+        for (key in dataList) k += Number(dataList[key]);
+    
+        k = Math.round(Math.sqrt(k)); 
+        let intervalo = Math.ceil(amplitude/k);
+       ///console.log(intervalo)
+        let xMaxParcial = xMin
+
+        while (xMaxParcial <= xMax) { 
+            let frequency = 0
             frequency = dataList.filter(value => value < xMaxParcial + intervalo && value >= xMaxParcial).length
 
            document.getElementById("tbody").innerHTML += 
-        `<tr>
-        <td>${xMaxParcial} &vdash; ${xMaxParcial += intervalo}</td>
-        <td>${frequency}</td>
-        </tr>` 
-              
+                `<tr>
+                <td>${xMaxParcial} &vdash; ${xMaxParcial += intervalo}</td>
+                <td>${frequency}</td>
+                </tr>` 
         } 
       
     }
     else {
         for (key in valuesQuantity) {
             document.getElementById("tbody").innerHTML += 
-                `<tr>
-                <td>${key}</td>
-                <td>${valuesQuantity[key]}</td>
-                </tr>`
+            `<tr>
+            <td>${key}</td>
+            <td>${valuesQuantity[key]}</td>
+            <td>${percentFr[key]}</td>
+            <td>${fac[key]}</td>
+            <td>${percentFac[key]}</td>
+            </tr>`
         }
     }
+
+}
+
+//Calcula a porcentagem das frequências
+function calcPercentFr(dataList, valuesQuantity) {
+    let total = dataList.length;
+    let frObject = {};
+
+    for (key in valuesQuantity) {
+        frObject[key] = `${((valuesQuantity[key]/total)*100).toFixed(2)}%`
+    }
+
+    return frObject
+}
+
+//Calcula a frequência acumulada
+function calcFac(valuesQuantity) {
+    acumulated = 0
+    acumulatedObj = {}
+
+    for (key in valuesQuantity) {
+        acumulated += valuesQuantity[key];
+        acumulatedObj[key] = acumulated;
+    }
+
+    return acumulatedObj
+}
+
+//Calcula a frequência acumulada percentual
+function calcPercentFac(percentFr) {
+    acumulated = 0;
+    acumulatedObj = {}
+
+    for (key in percentFr) {
+        acumulated += Number((percentFr[key]).slice(0,-1));
+
+        if (acumulated > 99.95) acumulated = 100
+        acumulatedObj[key] = `${acumulated.toFixed(2)}%`;
+    }
+
+    return acumulatedObj
 }
 
 /*  document.getElementById("tbody").innerHTML += 
