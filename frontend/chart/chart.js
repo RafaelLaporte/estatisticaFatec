cb1bdd9008f7549c32090e5ced2f870dd5d197ce
RafaelLaporte <rafanex@gmail.com>
2020-10-25 07:29:02 -0300

    Cores de fundo corrigidas

--- frontend/chart/chart.js
+++ frontend/chart/chart.js
@@ -1,11 +1,6 @@
 //Generate Charts
 function createChart(varType) {
 
-    // let graphTitle = document.createElement('div')
-    // graphTitle.className = 'graph-title'
-    // graphTitle.innerHTML = 'Gráfico:'
-    // document.getElementById('results-title').appendChild(graphTitle);
-
     let ctx = document.getElementById('chart').getContext('2d');
 
     if(window.bar != undefined)
