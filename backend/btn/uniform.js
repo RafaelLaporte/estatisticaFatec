ecb8669b0512106e9123839a55fee1d8a9ed0375
Estevao <estevao.bchaves@protonmail.com>
2020-10-29 10:11:58 -0300

    probabilidade uniforme atualizada

--- backend/btn/uniform.js
+++ backend/btn/uniform.js
@@ -6,30 +6,44 @@ function btnUniform() {
     let valueMax = Number(document.getElementById('varValueMax').value);
     let type = document.getElementById('uniformValues-select').value;
 
-    if (value < min || value > max) document.getElementById('result').innerHTML = `
-    O valor a ser analisado deve se encontrar no intervalo [min, max] estabelecido`
+    if (value < min || value > max){
+      document.getElementById('result').innerHTML = `
+      O valor a ser analisado deve se encontrar no intervalo [min, max] estabelecido`
+    } 
+    else if (max <= min){
+      document.getElementById('result').innerHTML = 
+      `O valor mínimo da distribuição deve ser menor que o valor máximo da distribuição`
+    } 
+    else if (type == 'between'){
 
-    if (valueMax < value ) document.getElementById('result').innerHTML = `
-    O valor mínimo do intervalo deve ser menor ou igual ao valor máximo do intervalo`
-
-
-    else{
-
-      //  [value, valueMax] = [valueMax, value]
-
+      if(valueMax < value){
+        document.getElementById('result').innerHTML = 
+        `O valor mínimo do intervalo deve ser menor que o valor máximo do intervalo`
+      } 
+      if(valueMax < min || valueMax > max){
+        document.getElementById('result').innerHTML = `
+        O valor a ser analisado deve se encontrar no intervalo [min, max] estabelecido`
+      }
+    }
+    else {
+        
+        let probability
         let distribution = 1/(max - min);
         let minProbability = (max - value)*distribution;
         let maxProbability = (value - min)*distribution;
-        let probability = value*distribution;
+        let probabilityBetween = (valueMax - value)*distribution;       
         let mean = (min + max)/2
         let standardDeviation = Math.sqrt(Math.pow(max - min, 2)/12);
         let variance = standardDeviation/mean
 
-        probabilityElements()
+        if(type == 'higherThan') probability = (minProbability*100).toFixed(2)
+        if(type == 'lessThan') probability = (maxProbability*100).toFixed(2)
+        if(type == 'between') probability = (probabilityBetween*100).toFixed(2)
 
-        // document.getElementById('probability').innerHTML = `Probabilidade de se obter o valor ${value}: ${(probability*100).toFixed(2)}%`
-        if(type == 'higherThan') document.getElementById('minimum-probability').innerHTML = `Probabilidade de se obter no mínimo ${value}: ${(minProbability*100).toFixed(2)}%`
-        if(type == 'lessThan') document.getElementById('maximum-probability').innerHTML = `Probabilidade de se obter no máximo ${value}: ${(maxProbability*100).toFixed(2)}%`
+
+        probabilityElements()
+        document.getElementById('result').innerHTML = ``
+        document.getElementById('probability').innerHTML = `Probabilidade: ${probability}%`
         document.getElementById('mean').innerHTML = `Média: ${mean}`
         document.getElementById('standard-deviation').innerHTML = `Desvio Padrão: ${standardDeviation.toFixed(2)}`
         document.getElementById('variance').innerHTML = `Variância: ${(variance*100).toFixed(2)}`
