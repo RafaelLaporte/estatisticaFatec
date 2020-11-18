9ed308d91c8122cb8ef5392a1272d4816e3c6bd9
RafaelLaporte <rafanex@gmail.com>
2020-11-18 15:23:37 -0300

    Modais da página correlação corrigidos

--- scripts/btn/btn-future.js
+++ scripts/btn/btn-future.js
@@ -13,14 +13,25 @@ function btnFuture() {
     let variable = document.getElementById('future-select').value;
     let value = Number(document.getElementById('future-value').value);
 
-    //The dependent variable value for some X value inputed.
+    //Verifying the inputs
+    if(!variable) {
+        result.innerHTML = '<h3> Insira a variável desejada </h3>';
+        return false
+    }
+
+    if(isNaN(value) || !value ) {
+        result.innerHTML = '<h3> Por favor, insira somente valores numéricos </h3>';
+        return false
+    }
+
+    //Calculating the dependent variable value for some X value inputed.
     if(variable == "X") {
         
         result.innerHTML = `<h3> y = ${(angular*value + linear).toFixed(2)} </h3>`;
         return false
     }
 
-    //The independent variable value for some Y value inputed.
+    //Calculating the independent variable value for some Y value inputed.
     if(variable == "Y") {
         if(angular == 0) {
             
@@ -33,10 +44,4 @@ function btnFuture() {
             return false
         }
     } 
-
-    //It verifies if the user filled the inputs to use the future-form
-    if(!variable || !value) {
-        result.innerHTML = '<h3> Insira a variável desejada </h3>';
-        return false
-    }
 }
